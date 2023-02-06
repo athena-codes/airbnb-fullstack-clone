57d8b257cdcdc75e12451536df7e318f5f5f60b2
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-02-06 17:11:03 -0500

    Add errorhandling mw for common errors + debugging

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -57,6 +57,45 @@ app.use(
 // verbs besides GET. This header will be used to validate the _csrf cookie to confirm that
 // the request comes from your site and not an unauthorized site.
 
-app.use(routes) // Connect all the routes
+app.use(routes) // Connect all the routes\
+
+// ****** ERROR HANDLING MIDDLEWWARE *******
+
+// Catch unhandled requests and forward to error handler.
+
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.")
+  err.title = 'Resource Not Found'
+  err.errors = ["The requested resource couldn't be found."]
+  err.status = 404
+  next(err)
+})
+
+const { ValidationError } = require('sequelize')
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map(e => e.message)
+    err.title = 'Validation error'
+  }
+  next(err)
+})
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500)
+  console.error(err)
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  })
+})
+
+
+
 
 module.exports = app
