d5b093e3b8face8961842f443938d786f3226284
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-02-07 17:50:08 -0500

    Add User authentication Express middleware

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,9 +1,22 @@
 // backend/routes/api/index.js
 const router = require('express').Router()
+const { setTokenCookie } = require('../../utils/auth.js')
+const { User } = require('../../db/models')
 
 // Test Route:
 router.post('/test', function (req, res) {
   res.json({ requestBody: req.body })
+})
+
+// GET /api/set-token-cookie
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: 'Demo-lition'
+    }
+  })
+  setTokenCookie(res, user)
+  return res.json({ user: user })
 })
 
 
