52b391fed2f7bd98ec0a6f72ddc253c4c8c75806
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-02-08 18:29:46 -0500

    Add User input validation on login backend endpoin

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -3,9 +3,24 @@ const router = express.Router()
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth')
 const { User } = require('../../db/models')
+const { check } = require('express-validator')
+const { handleValidationErrors } = require('../../utils/validation')
+
+
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+]
+
 
 // Log in
-router.post('/', async (req, res, next) => {
+router.post('/', validateLogin, async (req, res, next) => {
   const { credential, password } = req.body
 
   const user = await User.login({ credential, password })
@@ -19,15 +34,16 @@ router.post('/', async (req, res, next) => {
   }
 
   await setTokenCookie(res, user)
-
-  return res.json({
-    user: user
-  })
   // can also do this instead of await
   //     }).then(() => {
   //   setTokenCookie(res, user)
   // })
+
+  return res.json({
+    user: user
+  })
 })
+
 
 // Log out
 router.delete('/', (_req, res) => {
@@ -44,6 +60,8 @@ router.get('/', restoreUser, (req, res) => {
     })
   } else return res.json({ user: null })
 })
+
+
 
 
 module.exports = router
