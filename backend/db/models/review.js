52fd164f66427c2fbc91902923a3914c2ef1c634
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-02-15 17:15:43 -0500

    created reviews table + added constraints

---
+++ backend/db/models/review.js
@@ -0,0 +1,44 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Review extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  }
+  Review.init(
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE'
+      },
+      spotId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE'
+      },
+      review: {
+        type: DataTypes.STRING
+      },
+      stars: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          min: 1,
+          max: 5
+        }
+      }
+    },
+    {
+      sequelize,
+      modelName: 'Review'
+    }
+  )
+  return Review
+}
