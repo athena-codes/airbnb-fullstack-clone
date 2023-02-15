15948b838b616a89cf555d09fe1a5ad434d9a808
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-02-15 18:50:50 -0500

    created reviews table, added associations

---
+++ backend/db/models/reviewimage.js
@@ -0,0 +1,33 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class ReviewImage extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+      ReviewImage.belongsTo(models.Review, { foreignKey: 'reviewId' })
+    }
+  }
+  ReviewImage.init(
+    {
+      url: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      review_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE'
+      }
+    },
+    {
+      sequelize,
+      modelName: 'ReviewImage'
+    }
+  )
+  return ReviewImage
+}
