5154aa647c6f66e88b93e9cbd2a66e03c3d49b1b
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-02-14 20:36:30 -0500

    created Spots table and model

---
+++ backend/db/models/spot.js
@@ -0,0 +1,67 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Spot extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  }
+  Spot.init(
+    {
+      ownerId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      city: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      state: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      country: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lat: {
+        type: DataTypes.INTEGER
+      },
+      lng: {
+        type: DataTypes.INTEGER
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false
+      },
+      previewImage: {
+        type: DataTypes.TEXT
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      avgStarRating: {
+        type: DataTypes.INTEGER
+      }
+    },
+    {
+      sequelize,
+      modelName: 'Spot'
+    }
+  )
+  return Spot
+}
