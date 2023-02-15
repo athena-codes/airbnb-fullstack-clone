0ad0a4c3c130168d44223324d83c53bacdb47b52
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-02-14 20:56:45 -0500

    removed users association; debugging

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -9,7 +9,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate (models) {
       // define association here
-      Review.belongsTo(models.User, { foreignKey: 'userId' })
       Review.belongsTo(models.Spot, { foreignKey: 'spotId' })
     }
   }
