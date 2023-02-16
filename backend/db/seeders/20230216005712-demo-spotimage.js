aae431274934f462a0b27f5e0daf876bf06e5d6d
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-02-15 19:59:40 -0500

    created spotImages seeder

---
+++ backend/db/seeders/20230216005712-demo-spotimage.js
@@ -0,0 +1,43 @@
+'use strict'
+
+let options = {}
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    options.tableName = 'SpotImages'
+
+    return queryInterface.bulkInsert(options, [
+      {
+        url: 'https://images.pexels.com/photos/1428348/pexels-photo-1428348.jpeg?auto=compress&cs=tinysrgb&w=600',
+        preview: true,
+        spotId: 1,
+      },
+      {
+        url: 'https://images.pexels.com/photos/1428348/pexels-photo-1428348.jpeg?auto=compress&cs=tinysrgb&w=600',
+        preview: true,
+        spotId: 2,
+      },
+      {
+        url: 'https://images.pexels.com/photos/1428348/pexels-photo-1428348.jpeg?auto=compress&cs=tinysrgb&w=600',
+        preview: false,
+        spotId: 3,
+      },
+    ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = 'SpotImages'
+    const Op = Sequelize.Op
+    return queryInterface.bulkDelete(
+      options,
+      {
+        spotId: { [Op.in]: [1, 2, 3] }
+      },
+      {}
+    )
+  }
+}
