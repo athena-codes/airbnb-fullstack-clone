77ddd11f9c5ef9d56d1ad08708c3120208a80a94
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-02-23 16:04:37 -0500

    fixed routes/api/bookings.js

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -22,44 +22,41 @@ const {
 // ****** Get all of the Current User's Bookings *******
 // Require Authentication: true
 router.get('/current', requireAuth, async (req, res, next) => {
-    try {
-        const currentUserId = req.user.id
+  try {
+    const currentUserId = req.user.id
 
-        const allBookings = await Booking.findAll({
-          where: {
-            userId: currentUserId
-          }
-        })
-        let bookingsObj
-        //fix ordering
-        for (let booking of allBookings) {
-          const spot = await Spot.findOne({
-            where: {
-              ownerId: currentUserId
-            },
-            attributes: [
-              'id',
-              'ownerId',
-              'address',
-              'city',
-              'state',
-              'country',
-              'lat',
-              'lng',
-              'name',
-              'price',
-              'previewImage'
-            ]
-          })
-          bookingsObj = {
-              Bookings: allBookings,
-              Spot: spot
-          }
-        }
-        res.status(200).json(bookingsObj)
-    } catch (err) {
-        next(err)
+    const allBookings = await Booking.findAll({
+      where: {
+        userId: currentUserId
+      },
+      attributes: ['id', 'spotId', 'userId', 'startDate', 'endDate', 'createdAt', 'updatedAt']
+    })
+
+    for (let booking of allBookings) {
+      const spot = await Spot.findOne({
+        where: {
+          ownerId: currentUserId
+        },
+        attributes: [
+          'id',
+          'ownerId',
+          'address',
+          'city',
+          'state',
+          'country',
+          'lat',
+          'lng',
+          'name',
+          'price',
+          'previewImage'
+        ]
+      })
+      booking.Spot = spot
     }
+    return res.status(200).json(bookingsObj)
+  } catch (err) {
+    next(err)
+  }
 })
 
 module.exports = router
