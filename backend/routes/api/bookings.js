bc5e64ec950b3b88d5f8ba991b3e244d2f940525
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-02-23 17:18:18 -0500

    finalized routes/api/bookings.js

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -29,7 +29,15 @@ router.get('/current', requireAuth, async (req, res, next) => {
       where: {
         userId: currentUserId
       },
-      attributes: ['id', 'spotId', 'userId', 'startDate', 'endDate', 'createdAt', 'updatedAt']
+      attributes: [
+        'id',
+        'spotId',
+        'userId',
+        'startDate',
+        'endDate',
+        'createdAt',
+        'updatedAt'
+      ]
     })
 
     for (let booking of allBookings) {
@@ -52,9 +60,42 @@ router.get('/current', requireAuth, async (req, res, next) => {
         ]
       })
       booking.dataValues.Spot = spot
-      console.log('THIS IS THE BOOKING -->', booking)
     }
     return res.status(200).json({ Bookings: allBookings })
+  } catch (err) {
+    next(err)
+  }
+})
+
+// ****** Delete a Booking *******
+// Require Authentication: true
+router.delete('/:bookingId', requireAuth, async (req, res, next) => {
+  try {
+    const bookingId = req.params.bookingId
+    const booking = await Booking.findByPk(bookingId)
+    console.log(booking)
+
+    if (!booking) {
+      return res.status(404).json({
+        message: 'Booking not found',
+        statusCode: 404
+      })
+    }
+
+    const startDate = booking.startDate
+    const currentDate = new Date()
+    if (new Date(startDate) <= currentDate) {
+      return res.status(404).json({
+        message: 'Bookings that have been started can not be deleted',
+        statusCode: 403
+      })
+    }
+
+    await booking.destroy()
+    return res.status(200).json({
+      message: 'Successfully deleted',
+      statusCode: 200
+    })
   } catch (err) {
     next(err)
   }
