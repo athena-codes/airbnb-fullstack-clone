37e262d9bca3c03eea7fa388edc894b881e0904e
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-03-22 20:27:11 -0400

    successfully set up csrf protection

--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -26,4 +26,10 @@ export async function csrfFetch (url, options = {}) {
   return res
 }
 
+// call this to get the "XSRF-TOKEN" cookie, should only be used in development
+export function restoreCSRF () {
+  return csrfFetch('/api/csrf/restore')
+}
+
+
 export default csrfFetch;
