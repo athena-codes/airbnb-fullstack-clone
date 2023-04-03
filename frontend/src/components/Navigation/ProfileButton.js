1d7dbb86c72245c0cc9c398ab2f94a8ad420fd5f
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-04-02 21:55:46 -0400

    fixed css for homepage, added dropdown profile menu

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -4,26 +4,33 @@ import * as sessionActions from '../../store/session'
 
 function ProfileButton ({ user }) {
   const dispatch = useDispatch()
+  const [showMenu, setShowMenu] = useState(false)
 
   const logout = e => {
     e.preventDefault()
     dispatch(sessionActions.logout())
   }
 
-  const ulClassName = 'profile-dropdown'
+  const ulClassName = 'profile-dropdown' + (showMenu ? '' : ' hidden')
 
+  // had to add showMenu && inside of return for drop down effect 
   return (
     <>
-      <button>
+      <button onClick={() => setShowMenu(!showMenu)}>
         <i className='fas fa-user-circle' />
       </button>
-      <ul className={ulClassName}>
-        <li>{user.username}</li>
-        <li>
-          {user.firstName} {user.lastName}
-        </li>
-        <li>{user.email}</li>
-      </ul>
+      {showMenu && (
+        <ul className={ulClassName}>
+          <li>{user.username}</li>
+          <li>
+            {user.firstName} {user.lastName}
+          </li>
+          <li>{user.email}</li>
+          <li>
+            <button onClick={logout}>Log Out</button>
+          </li>
+        </ul>
+      )}
     </>
   )
 }
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,46 +1,47 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import ProfileButton from './ProfileButton'
-import * as sessionActions from '../../store/session'
-import './Navigation.css'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import ProfileButton from './ProfileButton';
+import * as sessionActions from '../../store/session';
+import './Navigation.css';
 
-function Navigation ({ isLoaded }) {
-  const sessionUser = useSelector(state => state.session.user)
-  const dispatch = useDispatch()
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
+  const dispatch = useDispatch();
 
-  const logout = e => {
-    e.preventDefault()
-    dispatch(sessionActions.logout())
-  }
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
 
-  let sessionLinks
+  let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
       <li>
         <ProfileButton user={sessionUser} />
-        <button onClick={logout}>Log Out</button>
       </li>
-    )
+    );
   } else {
     sessionLinks = (
       <li>
-        <NavLink to='/login'>Log In</NavLink>
-        <NavLink to='/signup'>Sign Up</NavLink>
+        <NavLink to="/login">Log In</NavLink>
+        <NavLink to="/signup">Sign Up</NavLink>
       </li>
-    )
+    );
   }
 
   return (
-    <ul>
-      <li>
-        <NavLink exact to='/'>
-          Home
-        </NavLink>
-      </li>
-      {isLoaded && sessionLinks}
-    </ul>
-  )
+    <nav>
+      <ul>
+        <li>
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+        </li>
+        {isLoaded && sessionLinks}
+      </ul>
+    </nav>
+  );
 }
 
-export default Navigation
+export default Navigation;
